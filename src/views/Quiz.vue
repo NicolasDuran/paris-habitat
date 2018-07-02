3b22ced82630f58c2918e49cf7f2a09ca0f6b95c
Nicolas DURAN <nicolas.duran@qarnot-computing.com>
2018-07-02 22:46:41 +0200

    Improve a bit the quiz graphic

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -15,10 +15,12 @@
             />
         </div>
         <div v-if="state == 'q1_g'">
-            <q-icon name="done_outline" /> Bravo !
+            <q-icon name="done_outline" class="goodanswer" /> <span class="answertxt">Bravo !</span>
+            <p>Une douche équivault à beaucoup d'eau</p>
         </div>
         <div v-if="state == 'q1_b'">
-            <q-icon name="highlight_off" /> Raté !
+            <q-icon name="highlight_off" class="badanswer" /> <span class="answertxt">Raté !</span>
+            <p>Une douche équivault à beaucoup d'eau</p>
         </div>
 
         <div v-if="state == 'q2'">
@@ -34,10 +36,12 @@
             />
         </div>
         <div v-if="state == 'q2_g'">
-            <q-icon name="done_outline" /> Bravo !
+            <q-icon name="done_outline" class="goodanswer" /> <span class="answertxt">Bravo !</span>
+            <p>Il faut toujours éteindre les lumières !</p>
         </div>
         <div v-if="state == 'q2_b'">
-            <q-icon name="highlight_off" /> Raté !
+            <q-icon name="highlight_off" class="badanswer" /> <span class="answertxt">Raté !</span>
+            <p>Il faut toujours éteindre les lumières !</p>
         </div>
 
         <div v-if="state == 'q3'">
@@ -53,10 +57,12 @@
             />
         </div>
         <div v-if="state == 'q3_g'">
-            <q-icon name="done_outline" /> Bravo !
+            <q-icon name="done_outline" class="goodanswer" /> <span class="answertxt">Bravo !</span>
+            <p>Fermer le robinet pendant qu'on se brosse les dents, c'est 22 litres d'eau potable économisés</p>
         </div>
         <div v-if="state == 'q3_b'">
-            <q-icon name="highlight_off" /> Raté !
+            <q-icon name="highlight_off" class="badanswer" /> <span class="answertxt">Raté !</span>
+            <p>Fermer le robinet pendant qu'on se brosse les dents, c'est 22 litres d'eau potable économisés</p>
         </div>
         <div v-if="isLast">
             Score: {{score}} / 3
@@ -65,6 +71,22 @@
         <q-btn v-if="isAnswerPage && !isLast" label="Question suivante" @click="nextQuestion()"/>
     </q-page>
 </template>
+
+<style>
+.goodanswer {
+  font-size: 60px;
+  color: green;
+}
+
+.answertxt {
+  font-size: 50px;
+}
+
+.badanswer {
+  font-size: 60px;
+  color: red;
+}
+</style>
 
 <script>
 export default {
