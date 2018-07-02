8fd7f0a3706a8aeac0fc3d514e274b63a627c5fa
Mion Etienne <mion_e@epita.fr>
2018-07-03 00:35:33 +0200

    add routes doc

--- data-api/routes/routes.js
+++ data-api/routes/routes.js
@@ -1,36 +1,36 @@
-
 var appRouter = function (app) {
+
 function getRandomFloat(min, max) {
-  console.log('test');
   return Math.random() * (max - min) + min;
 }
 
 function getRandomInt(min, max) {
-  console.log('test 1');
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
     app.get("/", function(req, res) {
-  console.log('test');
         res.json({
                 routes: [
                     "/api/graph1",
                     "/api/graph2",
+                    "/api/graph1/rt",
+                    "/api/graph2/rt",
                 ]
             });
     });
 
+    //get :graph history
     app.get("/api/:graph", function(req, res) {
         const json = require('../data/' + req.params.graph + '.json');
         res.json(json);
     });
 
+    //get graph1 real time
     app.get('/api/graph1/rt', function(req, res) {
-  console.log('test 2');
         res.json(getRandomInt(5, 10));
     });
-
+  
+    // get graph2 realtime
     app.get('/api/graph2/rt', function(req, res) {
-  console.log('test 3');
         res.json(getRandomInt(2, 18));
     });
 };
